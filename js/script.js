ce6f57f74d9bdd035dccbec64370310e8f2e8524
Nuno Martins <nunoormaker21@gmail.com>
2019-05-21 22:54:19 +0100

    Adição do Google Analytics; Adição da biblioteca Animate.css; Adição da biblioteca animatedModal.js; Adição da funcionalidade da seta da primeira secção; Adição da responsividade do site; Conserto de bugs;

--- js/script.js
+++ js/script.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
 		//options here
 		autoScrolling:true,
         scrollHorizontally: true,
-        dragAndMove: true,
+		dragAndMove: true,
+		verticalCentered: false,
         licenseKey: `p]yaoTk')T<GZKU6N;'i H|8p[L0C)kT]T~Gf+vVt53|e-jq`
 	});
 
-	/* Adicionar classe às setas */
+	// Adicionar classe às setas
 	$(".fp-controlArrow.fp-prev").addClass("fas fa-arrow-alt-circle-left");
 	$(".fp-controlArrow.fp-next").addClass("fas fa-arrow-alt-circle-right");
+
+	// Ao clicar na seta da primeira section, mover p/ baixo
+	$("#btn").click(function (e) { 
+		e.preventDefault();
+		$.fn.fullpage.moveTo(2);
+	});
 });
